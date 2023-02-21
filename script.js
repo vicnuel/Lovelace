c2614f35101b84926e393a19479b1a7af537f116
Victor Emanuel Sousa Lima <ve49550@gmail.com>
2023-02-21 10:49:54 -0300

    v1 beta correção 2

---
+++ script.js
@@ -0,0 +1,12 @@
+var button_products = document.getElementById("button_products")
+var button_blog = document.getElementById("button_blog")
+
+button_products.addEventListener("click", function () {
+    // redirect to products site
+    window.open("https://spokentext.lovelacetech.com.br", "_blank")
+})
+
+button_blog.addEventListener("click", function () {
+    // redirect to blog site
+    window.open("https://blog.spokentext.com.br", "_blank")
+})
